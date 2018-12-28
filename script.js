7b6df963ce7a6806417a60f29170ed5d8dda6a96
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-12-28 10:50:41 +0800

    init project

---
+++ script.js
@@ -0,0 +1,27 @@
+const Movie = props =>
+  React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.releaseDate),
+  ]);
+ 
+class App extends React.Component {
+    render() {
+      return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'React Movie App'),
+        React.createElement(Movie, {
+          name: 'Aquaman',
+          releaseDate: '2018-12-07'
+        }),
+        React.createElement(Movie, {
+          name: 'Bumblebee',
+          releaseDate: '2018-12-15',
+        }),
+        React.createElement(Movie, { 
+            name: 'Fantastic Beasts: The Crimes of Grindelwald', 
+            releaseDate: '2018-11-14' 
+        })
+      ]);
+    }
+  }
+
+  ReactDOM.render(React.createElement(App), document.getElementById('root'));
