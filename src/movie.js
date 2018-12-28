28371ac927060f66a165a188e1790795f0b010b0
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-12-28 15:12:03 +0800

    split components to their own module

---
+++ src/movie.js
@@ -0,0 +1,7 @@
+const Movie = props =>
+  React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.releaseDate)
+  ]);
+
+export default Movie;
