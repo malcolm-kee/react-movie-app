7a65f42e6925a1d3c73dc9564ba3d00315cb0a5e
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2018-12-30 21:38:34 +0800

    add show movies function

--- src/app.js
+++ src/app.js
@@ -2,16 +2,35 @@ import React from 'react';
 import Movie from './movie';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMovies: false
+    };
+    this.showMovies = this.showMovies.bind(this);
+  }
+
+  showMovies() {
+    this.setState({
+      showMovies: true
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>React Movie App</h1>
-        <Movie name="Aquaman" releaseDate="2018-12-07" />
-        <Movie name="Bumblebee" releaseDate="2018-12-15" />
-        <Movie
-          name="Fantastic Beasts: The Crimes of Grindelwald"
-          releaseDate="2018-11-14"
-        />
+        <button onClick={this.showMovies}>Show Movies</button>
+        {this.state.showMovies && (
+          <>
+            <Movie name="Aquaman" releaseDate="2018-12-07" />
+            <Movie name="Bumblebee" releaseDate="2018-12-15" />
+            <Movie
+              name="Fantastic Beasts: The Crimes of Grindelwald"
+              releaseDate="2018-11-14"
+            />
+          </>
+        )}
       </div>
     );
   }
