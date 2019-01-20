86c6fc8e2bb79b29fc56659fa9691902e2c42729
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-01-20 23:20:24 +0800

    add css

--- src/app.js
+++ src/app.js
@@ -14,10 +14,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>React Movie App</h1>
-        <button onClick={this.toggleMovies}>
-          {this.state.showMovies ? 'Hide' : 'Show'} Movies
-        </button>
+        <div className="title-bar">
+          <h1>React Movie App</h1>
+        </div>
+        <div className="button-container">
+          <button onClick={this.toggleMovies} className="button">
+            {this.state.showMovies ? 'Hide' : 'Show'} Movies
+          </button>
+        </div>
         {this.state.showMovies && (
           <>
             <Movie name="Aquaman" releaseDate="2018-12-07" />
